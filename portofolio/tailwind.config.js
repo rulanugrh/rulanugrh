5bb186e387b28fc08bbdf167f3c9c7f7ee99930b
rulanugrh <nextarituenak01@gmail.com>
2023-06-16 15:45:38 +0700

    fix: new styles

--- portofolio/tailwind.config.js
+++ portofolio/tailwind.config.js
@@ -12,7 +12,10 @@ module.exports = {
       },
       colors: {
         primary: '#0B2447',
-        secondary: '#F9D949'
+        secondary: '#DC3535',
+        linked: '#0A66C2',
+        insta: '#E4405F',
+        linux: '#FCC624'
       },
       fontFamily: {
         firaSans: "'Fira Sans', sans-serif",
